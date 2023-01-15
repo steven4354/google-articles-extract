de826bea1127b71a22d728ff35905ed6d0938332
steven4354 <steven4354@gmail.com>
2023-01-14 21:24:24 -0800

    make the read.py more like a human/browser

--- read.py
+++ read.py
@@ -3,6 +3,19 @@ from bs4 import BeautifulSoup
 import csv
 import os
 
+# TODO:
+# from fake_useragent import UserAgent
+# headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+# ua=UserAgent()
+# hdr = {'User-Agent': ua.random,
+#       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+#       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+#       'Accept-Encoding': 'none',
+#       'Accept-Language': 'en-US,en;q=0.8',
+#       'Connection': 'keep-alive'}
+
+hdr = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
 # Create the 'read' folder if it doesn't exist
 if not os.path.exists("read"):
     os.makedirs("read")
@@ -16,7 +29,7 @@ with open("articles.csv", "r") as file:
     # Iterate over each row in the CSV file
     for row in reader:
         url = row[0]
-        page = requests.get(url)
+        page = requests.get(url, headers=hdr)
         soup = BeautifulSoup(page.content, "html.parser")
 
         # Find all the text elements on the page
